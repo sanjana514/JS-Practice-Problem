3d9b72d9831a10f61936375d829bf4f3545a586f
sanjana <sanjanakazisupti@gmail.com>
2025-02-17 20:17:52 +0600

    Practice-1

--- if-else problems/p1.js
+++ if-else problems/p1.js
@@ -1,5 +1,5 @@
- // Take an input number and print "Odd" if it's odd, otherwise print "Even"
- var a=110;
+// Take an input number and print "Odd" if it's odd, otherwise print "Even"
+var a=110;
 if((a%2)==0){
     console.log('The num is Even');
 }
