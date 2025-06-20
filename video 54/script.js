ddbb66672038d64e46001bb8f278e9c02c254246
sanjana <sanjanakazisupti@gmail.com>
2025-06-20 19:45:44 +0600

    js practice

---
+++ video 54/script.js
@@ -0,0 +1,18 @@
+alert("Hello Sanjana!!");
+console.log("Hello Sanjana!!");
+
+var a=prompt("Enter a number");
+console.log("The number you entered is: " + a);
+var b=prompt("Enter another number");
+console.log("The second number you entered is: " + b);
+
+var istrue=confirm("Are you sure you want to leave this page and blast your computer ")
+if(istrue){
+    console.log("Computer is blasting");
+}
+else{
+    console.log("Computer is safe");
+}
+
+document.title = "Hey sanjana!"; //browser title bar
+document.body.style.backgroundColor = "yellow"; //background color of the page
